4cc1b36bdf59ff52e77132e3467d1e62d695e969
acyartugue <2015084955@ust-ics.mygbiz.com>
2018-11-25 23:49:35 +0800

    update

--- app/src/main/java/acy/artugue/com/remindmed/MainActivity.java
+++ app/src/main/java/acy/artugue/com/remindmed/MainActivity.java
@@ -7,32 +7,11 @@ import android.widget.Switch;
 import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
-
-
-    Switch switch1;
-    TextView textView;
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        textView = (TextView)findViewById(R.id.textView);
-        switch1 = (Switch)findViewById(R.id.switch1);
-
-        switch1.setOnCheckedChangeListener(this);
-    }
-
-    @Override
-    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-        if(switch1.isChecked()){
-            textView.setText("Switch on");
-        }
-        else{
-            textView.setText("Switch off");
-        }
-
     }
 }
