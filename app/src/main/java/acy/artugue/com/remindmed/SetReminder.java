771d0fa2e0c64afe5e2cd92ea5ccbfe211a4654f
acyartugue <2015084955@ust-ics.mygbiz.com>
2018-12-14 23:22:33 +0800

    init

--- app/src/main/java/acy/artugue/com/remindmed/SetReminder.java
+++ app/src/main/java/acy/artugue/com/remindmed/SetReminder.java
@@ -5,17 +5,39 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.CountDownTimer;
 import android.os.Vibrator;
 import android.provider.Settings;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+
+
 
 public class SetReminder extends BroadcastReceiver{
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        MediaPlayer mediaPlayer = MediaPlayer.create(context, Settings.System.DEFAULT_RINGTONE_URI);
-        mediaPlayer.start();
+        //MediaPlayer mediaPlayer = MediaPlayer.create(context, Settings.System.DEFAULT_RINGTONE_URI);
+        // mediaPlayer.start();
+
+        final MediaPlayer player = MediaPlayer.create(context, Settings.System.DEFAULT_RINGTONE_URI);
+        player.start();
+
+        CountDownTimer timer = new CountDownTimer(20000, 20000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                // Nothing to do
+            }
+
+            @Override
+            public void onFinish() {
+                if (player.isPlaying()) {
+                    player.stop();
+                    player.release();
+                }
+            }
+        };
+        timer.start();
+
         Vibrator v = (Vibrator) context.getSystemService(context.VIBRATOR_SERVICE);
         v.vibrate(10000);
 
@@ -23,3 +45,4 @@ public class SetReminder extends BroadcastReceiver{
 
 
 }
+
